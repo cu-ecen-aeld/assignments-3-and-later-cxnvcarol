f8ebe6fc48fbc0994e1cb225f914299df0c906db
Carol Naranjo <carol.naranjo@grandcentrix.net>
2025-02-17 00:08:11 +0100

    task1:validate username

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,7 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* harcoded_name=my_username();
+    char* loaded_name=malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(harcoded_name,loaded_name,"Names should match");
 }
