845bd46ccc0540f3da154e6e45d3c67bc15626b9
Carol Naranjo <cxnvcarol@gmail.com>
2025-02-22 13:25:01 +0100

    assignment 3 - part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,10 +34,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Done: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} -j 16 CROSS_COMPILE=${CROSS_COMPILE} all
+#    make ARCH=${ARCH} -j 16 CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
 fi
 
 echo "Adding the Image in outdir"
+# here was a todo missing.
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/.
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +54,12 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Done: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin var/log
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +67,52 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Done:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
-
-# TODO: Make and install busybox
-
+echo $PWD
+# Done: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Done: Add library dependencies to rootfs
+TOOLCHAIN_SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+echo "Copying from toolchain sysroot at $TOOLCHAIN_SYSROOT ..."
+cp -a $TOOLCHAIN_SYSROOT/lib/ld-linux-aarch64.so.1 lib
+cp -a $TOOLCHAIN_SYSROOT/lib64/libm.so.6 lib64
+cp -a $TOOLCHAIN_SYSROOT/lib64/libresolv.so.2 lib64
+cp -a $TOOLCHAIN_SYSROOT/lib64/libc.so.6 lib64
 
-# TODO: Make device nodes
+# Done: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Done: Clean and build the writer utility
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Done: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp finder* ${OUTDIR}/rootfs/home/.
+cp writer ${OUTDIR}/rootfs/home/.
+cp -r ../conf ${OUTDIR}/rootfs/home/conf
 
-# TODO: Chown the root directory
+# TODO: Chown the root directory //@authors, I'm assuming chown it to the root user? (I wish you were'nt this vague.)
+cd ${OUTDIR}
+#chown -R root:root rootfs
+## I wonder if chown for rootfs was necessary if the compressed is chowned anyway
 
-# TODO: Create initramfs.cpio.gz
+# Done: Create initramfs.cpio.gz
+cd rootfs
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ..
+gzip -f initramfs.cpio
+
