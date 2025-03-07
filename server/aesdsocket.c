4bda3ec7b2992298acb35a60abcf425f8ebc96fb
Carol Naranjo <cxnvcarol@gmail.com>
2025-03-07 14:53:19 +0100

    assignment 5 - part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -3,22 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <syslog.h>
-#include <stddef.h>
 #include <fcntl.h>
-// #include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-
-// #include <netinet/in.h>
-// #include <arpa/inet.h>
 #include <errno.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <netdb.h> // For getaddrinfo()
+#include <stdbool.h>
 
-#define PORT 9000
+#define PORT "9000" // Use string for getaddrinfo()
 #define FILE_PATH "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE 1024
+char *buffer;
 
 int sockfd = -1;
 int file_fd = -1;
@@ -26,191 +24,228 @@ int client_sockfd = -1;
 
 void cleanup_and_exit(int signal)
 {
-	// Log the signal caught
-	syslog(LOG_INFO, "Caught signal %d, exiting", signal);
+    // Log the signal caught
+    syslog(LOG_INFO, "Caught signal %d, exiting", signal);
 
-	// Close file descriptor if open
-	if (file_fd != -1)
-	{
-		close(file_fd);
-	}
+    // Close file descriptor if open
+    if (file_fd != -1)
+    {
+        close(file_fd);
+    }
 
-	// Close the client socket if connected
-	if (client_sockfd != -1)
-	{
-		close(client_sockfd);
-	}
+    // Close the client socket if connected
+    if (client_sockfd != -1)
+    {
+        close(client_sockfd);
+    }
 
-	// Close the server socket
-	if (sockfd != -1)
-	{
-		close(sockfd);
-	}
+    // Close the server socket
+    if (sockfd != -1)
+    {
+        close(sockfd);
+    }
 
-	// Delete the file
-	unlink(FILE_PATH);
+    // Delete file // TODO. Uncomment next comment.
+    remove(FILE_PATH);
+    free(buffer);
 
-	// Close syslog
-	closelog();
+    // Close syslog
+    closelog();
 
-	exit(0);
+    exit(0);
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-	// struct sockaddr_in server_addr, client_addr;
-	// socklen_t client_len = sizeof(client_addr);
-	char buffer[BUFFER_SIZE];
-	char *packet;
-	size_t packet_len = 0;
-	ssize_t n;
-	FILE *file;
-	// struct stat file_stat;
+    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
+    bool demonize = false;
 
-	// Open syslog
-	openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
+    if (argc > 1 && strcmp(argv[1], "-d") == 0)
+    {
+        syslog(LOG_DEBUG, "Should run as daemon");
+        demonize = true;
+    }
 
-	// Register signal handlers for graceful exit
-	signal(SIGINT, cleanup_and_exit);
-	signal(SIGTERM, cleanup_and_exit);
+    struct addrinfo hints, *res;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
 
-	// Step 1: Create socket
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1)
-	{
-		syslog(LOG_ERR, "Socket creation failed: %s", strerror(errno));
-		return -1;
-	}
+    // char *packet;
+    // ssize_t packet_len = 0;
+    ssize_t receivedsize;
 
-	// Step 2: Set server address. # TODO. instead use readaddr.
-	// memset(&server_addr, 0, sizeof(server_addr));
-	// server_addr.sin_family = AF_INET;
-	// server_addr.sin_addr.s_addr = INADDR_ANY;
-	// server_addr.sin_port = htons(PORT);
+    // Register signal handlers for graceful exit
+    signal(SIGINT, cleanup_and_exit);
+    signal(SIGTERM, cleanup_and_exit);
 
-	// Step 3: Bind socket
-	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-	{
-		syslog(LOG_ERR, "Bind failed: %s", strerror(errno));
-		close(sockfd);
-		return -1;
-	}
+    // Step 1: Set up hints for getaddrinfo()
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;       // Use IPv4
+    hints.ai_socktype = SOCK_STREAM; // Use stream sockets (TCP)
+    hints.ai_flags = AI_PASSIVE;     // For binding to all interfaces
 
-	// Step 4: Listen for incoming connections
-	if (listen(sockfd, 5) == -1)
-	{
-		syslog(LOG_ERR, "Listen failed: %s", strerror(errno));
-		close(sockfd);
-		return -1;
-	}
+    // Step 2: Get address info
+    int status = getaddrinfo(NULL, PORT, &hints, &res);
+    if (status != 0)
+    {
+        syslog(LOG_ERR, "getaddrinfo failed: %s", gai_strerror(status));
+        return -1;
+    }
 
-	syslog(LOG_INFO, "Server is listening on port %d", PORT);
+    // Step 3: Create socket
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1)
+    {
+        syslog(LOG_ERR, "Socket creation failed: %s", strerror(errno));
+        freeaddrinfo(res);
+        return -1;
+    }
+    // From https://beej.us/guide/bgnet/html/#getaddrinfoprepare-to-launch
+    // lose the pesky "Address already in use" error message.
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
+    {
+        perror("setsockopt");
+        exit(1);
+    }
 
-	while (1)
-	{
-		// Step 5: Accept client connection
-		client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
-		if (client_sockfd == -1)
-		{
-			syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
-			continue; // Continue to the next connection attempt
-		}
+    // Step 4: Bind socket to address
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) == -1)
+    {
+        syslog(LOG_ERR, "Bind failed: %s", strerror(errno));
+        freeaddrinfo(res);
+        close(sockfd);
+        return -1;
+    }
+    if (demonize)
+    {
+        pid_t pid = fork();
+        if (pid > 0)
+        {
+            // parentprocess
+            exit(0);
+        }
 
-		// Step 6: Log client IP address
-		syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(client_addr.sin_addr));
+        // child process continue
+        if (setsid() < 0)
+        {
+            perror("Failed to detach terminal session");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-		// Step 7: Open file for appending data
-		file_fd = open(FILE_PATH, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (file_fd == -1)
-		{
-			syslog(LOG_ERR, "File open failed: %s", strerror(errno));
-			close(client_sockfd);
-			continue; // Continue to the next connection attempt
-		}
+    // Step 5: Listen for a connection
+    if (listen(sockfd, 1) == -1)
+    {
+        syslog(LOG_ERR, "Listen failed: %s", strerror(errno));
+        freeaddrinfo(res);
+        close(sockfd);
+        return -1;
+    }
 
-		// Step 8: Receive data and process packets separated by newlines
-		while ((n = recv(client_sockfd, buffer, sizeof(buffer), 0)) > 0)
-		{
-			size_t i;
-			for (i = 0; i < n; i++)
-			{
-				// If newline is found, process the packet
-				if (buffer[i] == '\n')
-				{
-					packet = malloc(packet_len + 1);
-					if (!packet)
-					{
-						syslog(LOG_ERR, "Memory allocation failed");
-						packet_len = 0; // Reset and discard current packet
-						continue;
-					}
+    syslog(LOG_INFO, "Server is listening on port %s", PORT);
 
-					// Copy the received packet to the newly allocated buffer
-					memcpy(packet, buffer, packet_len);
-					packet[packet_len] = '\0'; // Null-terminate the string
+    // Free the address info as it's no longer needed
+    freeaddrinfo(res);
 
-					// Append packet to file
-					if (write(file_fd, packet, packet_len) == -1)
-					{
-						syslog(LOG_ERR, "File write failed: %s", strerror(errno));
-						free(packet);
-						break;
-					}
+    while (1)
+    {
+        // Step 6: Accept client connection
+        client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_sockfd == -1)
+        {
+            syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
+            continue; // Continue to the next connection attempt
+        }
 
-					// Append the newline character to the file as well
-					if (write(file_fd, "\n", 1) == -1)
-					{
-						syslog(LOG_ERR, "File write failed for newline: %s", strerror(errno));
-						free(packet);
-						break;
-					}
+        // Step 7: Log client IP address
+        syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(client_addr.sin_addr));
 
-					// Send the entire file content back to the client
-					lseek(file_fd, 0, SEEK_SET); // Reset the file pointer to the beginning
-					file = fopen(FILE_PATH, "r");
-					if (file)
-					{
-						while (fgets(buffer, sizeof(buffer), file) != NULL)
-						{
-							send(client_sockfd, buffer, strlen(buffer), 0);
-						}
-						fclose(file);
-					}
+        // Step 8: Open file for appending data // TODO. move outside the loop?
+        file_fd = open(FILE_PATH, O_RDWR | O_CREAT | O_APPEND, 0666); // rw-rw-rw-
+        if (file_fd == -1)
+        {
+            syslog(LOG_ERR, "File open failed: %s", strerror(errno));
+            close(client_sockfd);
+            continue; // Continue to the next connection attempt
+        }
+        syslog(LOG_DEBUG, "opened file ok");
 
-					free(packet);	// Free allocated memory for the packet
-					packet_len = 0; // Reset packet length for next packet
-				}
-				else
-				{
-					// Accumulate data in the buffer for the current packet
-					if (packet_len < BUFFER_SIZE - 1)
-					{
-						buffer[packet_len++] = buffer[i];
-					}
-					else
-					{
-						// If the packet is too long, discard it and reset
-						syslog(LOG_ERR, "Received packet too large, discarding it");
-						packet_len = 0;
-					}
-				}
-			}
-		}
+        // Step 9: Receive data and process packets separated by newlines
+        buffer = malloc(BUFFER_SIZE);
+        if (buffer == NULL)
+        {
+            perror("allocating initial memory");
+            exit(0);
+        }
+        ssize_t total_size = 0;
 
-		if (n == -1)
-		{
-			syslog(LOG_ERR, "Receive failed: %s", strerror(errno));
-		}
+        while ((receivedsize = recv(client_sockfd, buffer, BUFFER_SIZE, 0)) > 0)
+        {
+            char *pos_newline = strchr(buffer, '\n');
+            ssize_t size_to_save = 0;
+            if (!pos_newline)
+            {
+                // increase buffer size
+                size_to_save = BUFFER_SIZE;
+                syslog(LOG_DEBUG, "no break found\n");
+            }
+            else
+            {
+                size_to_save = pos_newline - buffer + 1; // 1 including the position of the breakline
+            }
+            total_size += size_to_save;
 
-		// Log the client disconnection
-		syslog(LOG_INFO, "Closed connection from %s", inet_ntoa(client_addr.sin_addr));
+            if (write(file_fd, buffer, size_to_save) == -1)
+            {
+                syslog(LOG_ERR, "File write failed: %s", strerror(errno));
+                free(buffer);
+                break;
+            }
+            if (pos_newline)
+            {
+                buffer = malloc(BUFFER_SIZE); // used now for reading.
 
-		// Close file and client socket
-		close(file_fd);
-		close(client_sockfd);
-	}
+                // Send the entire file content back to the client
+                if (lseek(file_fd, 0, SEEK_SET) == -1) // Reset the file pointer to the beginning
+                {
+                    perror("failed resetting file cursor.");
+                    exit(-1);
+                }
 
-	// Cleanup and exit (should never be reached due to the loop)
-	cleanup_and_exit(0);
-	return 0;
+                ssize_t read_size;
+                while ((read_size = read(file_fd, buffer, BUFFER_SIZE)) > 0)
+                {
+                    syslog(LOG_DEBUG, "sending back %ld chars'\n", read_size);
+                    send(client_sockfd, buffer, read_size, 0);
+                    buffer = malloc(BUFFER_SIZE);
+                }
+                syslog(LOG_DEBUG, "Sent all back.");
+                close(client_sockfd);
+                syslog(LOG_INFO, "Closed connection from %s", inet_ntoa(client_addr.sin_addr));
+                break;
+            }
+            else
+            {
+                buffer = malloc(BUFFER_SIZE);
+                if (buffer == NULL)
+                {
+                    perror("While allocating extra memory");
+                    exit(0);
+                }
+                syslog(LOG_DEBUG, "allocated extra for packet");
+            }
+        }
+
+        if (receivedsize == -1)
+        {
+            syslog(LOG_ERR, "Receive failed: %s", strerror(errno));
+        }
+        else
+        {
+            syslog(LOG_DEBUG, "Last received is %ld", receivedsize);
+        }
+    }
+    cleanup_and_exit(0);
+    return 0;
 }
